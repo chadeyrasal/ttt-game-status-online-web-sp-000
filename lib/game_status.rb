883d23bd4bdbd0ce34efabacbe23c3d86e71dd47
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-21 13:06:37 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,13 +7,13 @@ end
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
 def won?(board)
-  WIN_COMBINATIONS.select do |win_combination|
-    if win_combination.all?{|position| position_taken?(board, position) == true} == false
-      false
-    else
+  WIN_COMBINATIONS.each do |win_combination|
+    if win_combination.all?{|position| position_taken?(board, position) == true} == true
       if board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]]
         return win_combination
       end
+    else
+      false
     end
   end
 end
